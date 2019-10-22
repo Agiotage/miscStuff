11e47bc82124732ec3cbbc6779f9f7cd78c1bca5
Agiotage <56834608+Agiotage@users.noreply.github.com>
2019-10-22 10:22:51 +0300

    brackets

--- factorial.php
+++ factorial.php
@@ -2,7 +2,8 @@
 
 $n = 10;
 
-function factorial($n) {
+function factorial($n) 
+{
     if ($n == 1) {
         return 1;
     } else {
@@ -13,3 +14,4 @@ function factorial($n) {
 $result = factorial($n);
 
 print_r("Факториал числа {$n} равняется {$result}.");
+
